657f69bc44f14d3540a46dfa6b4be5c75379564e
Michael Yeung <qq1187578@gmail.com>
2025-07-29 18:34:57 +0100

    Add dark mode toggle

--- script.js
+++ script.js
@@ -116,5 +116,18 @@ filters.forEach((btn) => {
     });
 });
 
+const toggleBtn = document.getElementById("toggle-theme");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const isDark = document.body.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
 
 loadTasks(); // load tasks on page load
